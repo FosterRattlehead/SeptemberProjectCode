bfb8f8e2627522669fe8bffdcfdeed6c9170004a
Daren Shamoun <darenshamoun@gmail.com>
2022-09-30 02:03:35 -0700

    fixed display of the PostLoginScreen.java

--- src/main/java/BankAccount2/PostLoginScreen.java
+++ src/main/java/BankAccount2/PostLoginScreen.java
@@ -1,5 +1,6 @@
 package BankAccount2;
 
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,16 +12,16 @@ public class PostLoginScreen extends UserInterfaceJavaFX
     public static Scene display(Stage mainStage)
     {
         GridPane postLoginPane = new GridPane();
-        postLoginPane.setPadding(new Insets(60, 60, 60, 60));
-        postLoginPane.setHgap(10);
-        postLoginPane.setVgap(10);
+        postLoginPane.setPadding(new Insets(20, 20, 20, 20));
+        postLoginPane.setHgap(20);
+        postLoginPane.setVgap(20);
 
-        Scene postLoginScene = new Scene(postLoginPane, 360, 360);
+        Scene postLoginScene = new Scene(postLoginPane, 200, 200);
 
 
         //deposit button
         Button depositButton = new Button("Deposit");
-        GridPane.setMargin(depositButton, new Insets(10, 10, 10, 10));
+        GridPane.setHalignment(depositButton, HPos.CENTER);
         depositButton.setOnAction(e ->
         {
             mainStage.setScene(DepositScreen.display(mainStage));
@@ -28,7 +29,7 @@ public class PostLoginScreen extends UserInterfaceJavaFX
 
         //withdrawal button
         Button withdrawButton = new Button("Withdraw");
-        GridPane.setMargin(withdrawButton, new Insets(10, 10, 10, 10));
+        GridPane.setHalignment(withdrawButton, HPos.CENTER);
         withdrawButton.setOnAction(e ->
         {
             mainStage.setScene(WithdrawalScreen.display(mainStage));
@@ -36,15 +37,15 @@ public class PostLoginScreen extends UserInterfaceJavaFX
 
         //logout button function
         Button buttonLogout = new Button("Logout");
-        GridPane.setMargin(withdrawButton, new Insets(10, 10, 10, 10));
+        GridPane.setHalignment(buttonLogout, HPos.CENTER);
         buttonLogout.setOnAction(e ->
         {
             mainStage.setScene(LoginScreen.display(mainStage));
         });
 
-        postLoginPane.add(depositButton, 0,0);
-        postLoginPane.add(withdrawButton,0,1);
-        postLoginPane.add(buttonLogout, 0,3);
+        postLoginPane.add(depositButton, 2,0);
+        postLoginPane.add(withdrawButton,2,1);
+        postLoginPane.add(buttonLogout, 2,2);
 
         mainStage.setScene(postLoginScene);
         mainStage.setTitle("The Totally NOT Fraudulent Bank");
